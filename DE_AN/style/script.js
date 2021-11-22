46ea987f6cdc1eba689639ad1314710bf3840bce
phamthainguyen0703 <79566684+phamthainguyen0703@users.noreply.github.com>
2021-11-22 17:15:57 +0700

    upload

---
+++ DE_AN/style/script.js
@@ -0,0 +1,26 @@
+// // search-icon
+// const clearInput = () => {
+//     const input = document.getElementsByTagName("input")[0];
+//     input.value = "";
+// }
+
+  
+//   const clearBtn = document.getElementById("clear-btn");
+//   clearBtn.addEventListener("click", clearInput);
+  
+//header animation
+
+// const Header = document.querySelector(".header")
+// window.addEventListener("scroll",function(){
+//     const x = this.pageYOffset;
+//     console.log(x);
+//     if (x>80){
+//         Header.classList.add("active")
+//     }
+//     else{
+//         Header.classList.remove("active")
+//     }
+// });
+// window.addEventListener("scroll", () =>{
+//     console.log("aaa");
+// });
